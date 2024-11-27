d338f09f835f2e424d07b1a88e3ff6f2bf9d8e07
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-11-27 18:59:56 +0500

    Update the Axios Instance

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Ensure this matches your backend URL
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  // withCredentials: true, // Remove this line
 });
 
+// Add the Authorization header to each request
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
