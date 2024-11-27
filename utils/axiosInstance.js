4e02273190bb7ecbfffdfe045cbad9d1a8e771a0
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-11-27 19:26:45 +0500

    Update the Axios Instance

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Should be https://sayings-backend.onrender.com/api
-  // withCredentials: true, // Only if you need to send cookies
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Now includes /api at the end
 });
 
-// Add Authorization header if using JWT tokens
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
